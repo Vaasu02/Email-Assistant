b1b9cee53db80054217473b94bd9d18f06c59ac0
Vaasu02 <vasuchourasia625@gmail.com>
2025-03-20 21:54:10 +0530

    added-deployed-link

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://email-assistant-a2ge.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
